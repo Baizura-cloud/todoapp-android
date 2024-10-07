35acb14d0278cfc78ee1f56e9306a123d5bc6b0f
Baizura <53967791+Baizura-cloud@users.noreply.github.com>
2024-10-07 15:48:04 +0800

    initial

---
+++ app/index.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function Index() {
+  const [name, setName] = useState<string>("");
+  const [status, setStatus] = useState<boolean>(false);
+  interface Task {
+    id: number;
+    name: string;
+    status: boolean;
+  }
+  const [taskList, setTaskList] = useState<Task[]>([
+    {
+      id: 0,
+      name: "Grocery",
+      status: false,
+    },
+    {
+      id: 1,
+      name: "Driving Class",
+      status: true,
+    },
+  ]);
+  const [activeTask, setActiveTask] = useState({});
+
+  const handleChange = (name?: string, status?: boolean) => {
+    console.log(name);
+    if (name) {
+      setName(name);
+    }
+    if (status) {
+      setStatus(status);
+    }
+  };
+  const handleSubmit = () => {
+    const curid: number = taskList.length + 1;
+    const newTask: Task[] = [{ id: curid, name: name, status: status }];
+    setTaskList((prevTask) => [...prevTask, ...newTask]);
+  };
+  const renderList = () => {
+    return (
+      <View>
+        {taskList
+          ? taskList.map((data) => (
+              <View key={data.id}>
+                <Text>{data.name}</Text>
+              </View>
+            ))
+          : null}
+      </View>
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={name}
+        placeholder="Enter your task"
+        onChangeText={handleChange}
+      />
+      <Button title="Submit" onPress={handleSubmit} />
+      {renderList()}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  multiInput: {
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
